f5a9aab90cf1a5bc7acdb9e5b9f57ae7b2ca08b6
Heather Bailey <mcdancerson@gmail.com>
2021-02-06 13:05:29 -0700

    Week 4 Assignment

--- js/scripts.js
+++ js/scripts.js
@@ -14,5 +14,14 @@ $(function()
             $("#carouselButton").children("i").addClass("fa-pause");
         }
     });
+    $("#reserveButton").click(function(){
+        $("#reserveModal").modal("show");
+
+    });
+
+    $("#loginButton").click(function(){
+        $("#loginModal").modal("show");
+
+    });
 
 });

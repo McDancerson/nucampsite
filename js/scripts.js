da84a4c4c6fbf9a6be83a4e9374c23a90ab238b0
Heather Bailey <mcdancerson@gmail.com>
2021-03-13 05:39:20 -0700

    NPM Scripts Part 2

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,27 @@
 
-$(function() 
-{
-    $(".carousel").carousel({interval: 2000});
-    $("#carouselButton").click(function()
-    {
-        if($("#carouselButton").children("i").hasClass("fa-pause")){
+$(function () {
+    $(".carousel").carousel({ interval: 2000 });
+    $("#carouselButton").click(function () {
+        if ($("#carouselButton").children("i").hasClass("fa-pause")) {
             $(".carousel").carousel("pause");
             $("#carouselButton").children("i").removeClass("fa-pause");
             $("#carouselButton").children("i").addClass("fa-play");
-        }else {
+        } else {
             $(".carousel").carousel("cycle");
             $("#carouselButton").children("i").removeClass("fa-play");
             $("#carouselButton").children("i").addClass("fa-pause");
         }
     });
-    $("#reserveButton").click(function(){
-        $("#reserveModal").modal("show");
+});
 
-    });
-
-    $("#loginButton").click(function(){
-        $("#loginModal").modal("show");
-
-    });
+$("#reserveButton").click(function () {
+    $("#reserveModal").modal("show");
 
 });
+
+$("#loginButton").click(function () {
+    $("#loginModal").modal("show");
+
+});
+
+
